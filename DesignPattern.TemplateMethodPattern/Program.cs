9071832c21da94c0a2c72b55ea03967fd98c379c
unknown <duje.miljevic@gmail.com>
2024-12-26 14:38:46 +0100

    template method pattern

---
+++ DesignPattern.TemplateMethodPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using DesignPattern.TemplateMethodPattern.Base;
+using DesignPattern.TemplateMethodPattern.Levels;
+
+namespace DesignPattern.TemplateMethodPattern;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        GameLevel dungeon = new DungeonLevel();
+        dungeon.LoadLevel();
+
+        GameLevel forest = new ForestLevel();
+        forest.LoadLevel();
+    }
+}
