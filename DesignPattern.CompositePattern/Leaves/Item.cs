f765554feaac16254900053135e0d137cc47fb3a
unknown <duje.miljevic@gmail.com>
2024-12-28 13:07:05 +0100

    composite pattern

---
+++ DesignPattern.CompositePattern/Leaves/Item.cs
@@ -0,0 +1,18 @@
+﻿using DesignPattern.CompositePattern.Components;
+
+namespace DesignPattern.CompositePattern.Leaves;
+
+public class Item : IGameComponent
+{
+    public string Name { get; }
+
+    public Item(string name)
+    {
+        Name = name;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Item: {Name}");
+    }
+}
---
+++ DesignPattern.CompositePattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using DesignPattern.CompositePattern.Composites;
+using DesignPattern.CompositePattern.Leaves;
+
+namespace DesignPattern.CompositePattern;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Item sword = new Item("Sword");
+        Item helmet = new Item("Helmet");
+        Item potion = new Item("Potion");
+
+        Container chest = new Container("Chest");
+        Container backpack = new Container("Backpack");
+
+        chest.AddItem(sword);
+        chest.AddItem(helmet);
+        backpack.AddItem(potion);
+
+        Container mainInventory = new Container("Main Inventory");
+        mainInventory.AddItem(chest);
+        mainInventory.AddItem(backpack);
+
+        mainInventory.Display();
+    }
+}
